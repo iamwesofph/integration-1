2c72af983218b791b42173798cfb4a768040d626
iamwesofph <piuswesley@gmail.com>
2023-09-19 13:34:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// const personsRouter = require("./controllers/persons");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(config.MONGODB_URI);
+        logger.info("Connected to MongoDB");
+    } catch (error) {
+        logger.error("Error connecting to MongoDB:", error.message);
+    }
+};
+connectToMongoDB();
+
+app.use(cors());
+// app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+// app.use("/", personsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
