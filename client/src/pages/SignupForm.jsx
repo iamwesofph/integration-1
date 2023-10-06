7badaae6ccbf77a3d7412d9bfe3e94c74d972ff9
iamwesofph <piuswesley@gmail.com>
2023-10-06 19:05:02 +0800

    Add: Disable signup page after submit + spinner animation

--- client/src/pages/SignupForm.jsx
+++ client/src/pages/SignupForm.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import userService from "../services/users";
-import emailService from "../services/emailService";
 import { useNavigate } from "react-router-dom";
 
 const SignupForm = ({ setNotification }) => {
     const navigate = useNavigate();
+    const [isDisabled, setIsDisabled] = useState(false);
     const [formData, setFormData] = useState({
         displayName: "",
         email: "",
@@ -26,6 +26,7 @@ const SignupForm = ({ setNotification }) => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        setIsDisabled(!isDisabled);
 
         const loggedUserToken = window.localStorage.getItem("loggedUserToken");
         const headerConfig = {
@@ -37,12 +38,12 @@ const SignupForm = ({ setNotification }) => {
 
         try {
             await userService.create(formData);
-            await emailService.sendEmail(headerConfig);
-            setNotification({ message: `You're signed up as ${formData.displayName}! Now check your inbox to verify your email`, type: "success" });
+            setNotification({ message: `You're signed up as ${formData.displayName}!  Redirecting to login page...`, type: "success" });
             setTimeout(() => {
                 setNotification(null);
+                navigate("/login");
+                navigate(0);
             }, 5000);
-            navigate("/");
         } catch (error) {
             console.log(`HANDLESUBMIT ERROR ${error}`);
         }
@@ -54,47 +55,53 @@ const SignupForm = ({ setNotification }) => {
             <form className="sm:w-[400px] mx-auto" onSubmit={handleSubmit}>
                 <label>
                     <span className="block text-sm font-medium text-white">Display Name</span>
-                    <input type="text" name="displayName" value={formData.displayName} onChange={handleChange} className="mt-1 mb-2 px-3 py-2 bg-white text-black border-2 shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1  valid:border-green-400 " placeholder="John Wick" autoComplete="displayName" required autoFocus />
+                    <input type="text" name="displayName" value={formData.displayName} onChange={handleChange} className="mt-1 mb-2 px-3 py-2 bg-white text-black border-2 shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1  valid:border-green-400 disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-300" placeholder="John Wick" autoComplete="displayName" required autoFocus disabled={isDisabled} />
                 </label>
 
                 <label>
                     <span className="block text-sm font-medium text-white">Email Address</span>
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} className="mt-1 mb-2 px-3 py-2 bg-white text-black border-2 shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1  valid:border-green-400 " placeholder="john@example.com" required />
+                    <input type="email" name="email" value={formData.email} onChange={handleChange} className="mt-1 mb-2 px-3 py-2 bg-white text-black border-2 shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1  valid:border-green-400 disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-300 " placeholder="john@example.com" required disabled={isDisabled} />
                 </label>
 
                 <label>
                     <span className="block text-sm font-medium text-white">Password</span>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} className="mt-1 mb-2 px-3 py-2 bg-white text-black border-2 shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1  valid:border-green-400 " placeholder="JohnWick@123" autoComplete="current-password" required />
+                    <input type="password" name="password" value={formData.password} onChange={handleChange} className="mt-1 mb-2 px-3 py-2 bg-white text-black border-2 shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1  valid:border-green-400 disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-300" placeholder="JohnWick@123" autoComplete="current-password" required disabled={isDisabled} />
                 </label>
 
-                {/* <label>
-                    <span className="block text-sm font-medium text-white">Confirm Password</span>
-                    <input type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} className="mt-1 mb-2 px-3 py-2 bg-white text-black border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="JohnWick@123" required />
-                </label> */}
                 <label>
                     <span>Confirm Password</span>
                     <input
-                        className="mt-1 mb-2 px-3 py-2 bg-white text-black border-2 shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 valid:border-green-400"
+                        className="mt-1 mb-2 px-3 py-2 bg-white text-black border-2 shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 valid:border-green-400 disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-300"
                         name="confirmPassword"
                         type="password"
-                        value={formData.confirmPassword}
-                        onChange={handleChange}
+                        // value={formData.confirmPassword}
+                        // onChange={handleChange}
                         placeholder="Confirm Password"
                         required
                         pattern={`^${formData.password}$`} // Use a regular expression to match the password
                         // pattern={formData.password} // Check if it matches the password
                         title="Passwords must match" // Custom error message
+                        disabled={isDisabled}
                     />
                 </label>
 
                 <label>
                     <span className="block text-sm font-medium text-white">Profile Photo</span>
-                    <input type="file" name="uploadPhoto" onChange={handleChange} className="mt-1 mb-2 px-3 py-2 bg-white text-black border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    <input type="file" name="uploadPhoto" onChange={handleChange} className="mt-1 mb-2 px-3 py-2 bg-white text-black border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" disabled={isDisabled} />
                 </label>
-
-                <button className="bg-cyan-400 hover:bg-cyan-500 text-white font-bold py-2 px-4 rounded focus:outline-none mr-2" type="submit">
-                    Sign Up
-                </button>
+                {isDisabled ? (
+                    <button disabled className="bg-cyan-400 hover:bg-cyan-500 text-white font-bold py-2 px-4 rounded focus:outline-none mr-2" type="submit">
+                        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        Processing
+                    </button>
+                ) : (
+                    <button className="bg-cyan-400 hover:bg-cyan-500 text-white font-bold py-2 px-4 rounded focus:outline-none mr-2" type="submit">
+                        Sign Up
+                    </button>
+                )}
             </form>
         </>
     );
